2d83a48b3779665eb83164d124eee0e06dd44311
rohit737363 <chougalerohit73@gmail.com>
2023-03-30 15:50:46 +0530

    codepush

---
+++ src/main/java/com/example/travellerms/TravellerMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.travellerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellerMsApplication.class, args);
+	}
+
+}
